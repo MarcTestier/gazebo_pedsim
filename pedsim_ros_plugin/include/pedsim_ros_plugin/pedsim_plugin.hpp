e8ec529545640484ae3525bba446cd6d17c19566
“Marc <marc.testier@gmail.com>
2019-09-12 18:00:26 +0800

    Added gazebo model to agents and obstacles

--- pedsim_ros_plugin/include/pedsim_ros_plugin/pedsim_plugin.hpp
+++ pedsim_ros_plugin/include/pedsim_ros_plugin/pedsim_plugin.hpp
@@ -12,6 +12,8 @@
 
 #include "pedsim/ped_includes.h"
 
+#include "pedsim_ros_plugin/PedSimInit.h"
+
 
 namespace gazebo
 {
@@ -26,14 +28,27 @@ public:
     void OnUpdate();
 
 private:
+    void initROSNode();
+
     void initPedSim();
 
     void createAgentModel(int i, Ped::Tvector pos);
+    void createObstacleModel(std::string name, double ax, double ay, double bx, double by);
+
+    bool PedSimInitService(
+        pedsim_ros_plugin::PedSimInit::Request &req,
+        pedsim_ros_plugin::PedSimInit::Response &res
+    );
 
 private:
     /// Gazebo variables
     physics::WorldPtr world;
     event::ConnectionPtr updateConnection;
+    std::vector<physics::ModelPtr> agent_model_array;
+
+    /// ROS variables
+    std::unique_ptr<ros::NodeHandle> rosNode;
+    ros::ServiceServer pedSimInitService;
 
     /// PedSim variables
     std::shared_ptr<Ped::Tscene> pedscene;
@@ -42,6 +57,8 @@ private:
     std::shared_ptr<Ped::Tobstacle> obstacle;
     std::vector<std::shared_ptr<Ped::Tagent>> agent_array;
 
+    /// Flags
+    bool isPedSimInit;
 };
 
 GZ_REGISTER_WORLD_PLUGIN(PedSimPlugin)
--- pedsim_ros_plugin/src/pedsim_plugin.cpp
+++ pedsim_ros_plugin/src/pedsim_plugin.cpp
@@ -2,7 +2,7 @@
 
 namespace gazebo
 {
-    PedSimPlugin::PedSimPlugin() : WorldPlugin()
+    PedSimPlugin::PedSimPlugin() : WorldPlugin(), isPedSimInit(false)
     {
     }
 
@@ -28,68 +28,133 @@ namespace gazebo
         this->world = _world;
         this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&PedSimPlugin::OnUpdate, this));
 
-        this->initPedSim();
+        this->initROSNode();
+    }
+
+    void PedSimPlugin::initROSNode(){
+        // Create ros node and publish stuff there!
+        this->rosNode.reset(new ros::NodeHandle("pedsim_plugin_ros_node"));
+
+        this->pedSimInitService = this->rosNode->advertiseService(this->world->Name() + "/pedSimInitService", &PedSimPlugin::PedSimInitService, this);
     }
 
     void PedSimPlugin::initPedSim(){
         ROS_INFO_STREAM("Initializing PedSim");
+
+        this->world->SetPaused(true);
+
         // Setup
         this->pedscene = std::make_shared<Ped::Tscene>(-200, -200, 400, 400);
         this->w1 = std::make_shared<Ped::Twaypoint>(-100, 0, 24);
         this->w2 = std::make_shared<Ped::Twaypoint>(+100, 0, 12);
-        this->obstacle = std::make_shared<Ped::Tobstacle>(0, -50,  0, +50);
+        this->obstacle = std::make_shared<Ped::Tobstacle>(0, -5,  0.05, +5);
+        this->createObstacleModel("obs1", 0, -5,  0.05, +5);
         pedscene->addObstacle(obstacle.get());
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < 20; i++) {
             std::shared_ptr<Ped::Tagent> agent = std::make_shared<Ped::Tagent>();
             agent_array.push_back(agent);
             agent->addWaypoint(w1.get());
             agent->addWaypoint(w2.get());
             agent->setPosition(-50 + rand()/(RAND_MAX/80)-40, 0 + rand()/(RAND_MAX/20) -10, 0);
+            agent->setfactorsocialforce(2.0);
+            agent->setfactorobstacleforce(2.0);
+            //agent->setfactorlookaheadforce(5.0);
             this->createAgentModel(i, agent->getPosition());
             this->pedscene->addAgent(agent.get());
         }
 
+        this->world->SetPaused(false);
+
         ROS_INFO_STREAM("PedSim initialized");
     }
 
-    void PedSimPlugin::createAgentModel(int i, Ped::Tvector pos){
-        // Insert a sphere model from string
-        sdf::SDF sphereSDF;
-        sphereSDF.SetFromString(
+
+    void PedSimPlugin::createObstacleModel(std::string name, double ax, double ay, double bx, double by){
+       // Insert a sphere model from string
+        sdf::SDF obsSDF;
+        obsSDF.SetFromString(
         "<sdf version ='1.4'>\
-            <model name ='agent'>\
-                <pose>" + std::to_string(pos.x) + " " + std::to_string(pos.y) + " " + std::to_string(pos.z) + " 0 0 0</pose>\
+            <model name ='" + name + "'>\
+                <static>true</static>\
+                <pose>" + std::to_string(ax+bx) + " " + std::to_string(ay+by) + " 0.5 0 0 0</pose>\
                 <link name ='link'>\
-                <pose>0 0 .2 0 0 0</pose>\
-                <collision name ='collision'>\
-                    <geometry>\
-                    <cylinder><radius>0.2</radius><length>1</length></cylinder>\
-                    </geometry>\
-                </collision>\
-                <visual name='visual'>\
-                    <geometry>\
-                    <cylinder><radius>0.2</radius><length>1</length></cylinder>\
-                    </geometry>\
-                </visual>\
+                    <pose>0 0 0 0 0 0</pose>\
+                    <collision name ='collision'>\
+                        <geometry>\
+                            <box><size>" + std::to_string((std::abs(ax)+std::abs(bx))) + " " + std::to_string((std::abs(ay)+std::abs(by))) + " 1</size></box>\
+                        </geometry>\
+                    </collision>\
+                    <visual name='visual'>\
+                        <geometry>\
+                            <box><size>" + std::to_string((std::abs(ax)+std::abs(bx))) + " " + std::to_string((std::abs(ay)+std::abs(by))) + " 1</size></box>\
+                        </geometry>\
+                    </visual>\
                 </link>\
             </model>\
             </sdf>");
-        // Demonstrate using a custom model name.
-        sdf::ElementPtr model = sphereSDF.Root()->GetElement("model");
-        model->GetAttribute("name")->SetFromString("agent " + std::to_string(i));
-        this->world->InsertModelSDF(sphereSDF);
+        this->world->InsertModelSDF(obsSDF);
+    }
+
+    void PedSimPlugin::createAgentModel(int i, Ped::Tvector pos){
+       // Insert a sphere model from string
+        sdf::SDF agentSDF;
+        agentSDF.SetFromString(
+        "<sdf version ='1.4'>\
+            <model name ='agent" + std::to_string(i) + "'>\
+                <static>true</static>\
+                <pose>" + std::to_string(pos.x) + " " + std::to_string(pos.y) + " " + std::to_string(pos.z) + " 0 0 0</pose>\
+                <link name ='link'>\
+                    <pose>0 0 .2 0 0 0</pose>\
+                    <collision name ='collision'>\
+                        <geometry>\
+                            <cylinder><radius>0.15</radius><length>1</length></cylinder>\
+                        </geometry>\
+                    </collision>\
+                    <visual name='visual'>\
+                        <geometry>\
+                            <cylinder><radius>0.15</radius><length>1</length></cylinder>\
+                        </geometry>\
+                    </visual>\
+                </link>\
+            </model>\
+            </sdf>");
+
+        this->world->InsertModelSDF(agentSDF);
+
+
+        // Wait for the entity to spawn
+        while (!this->world->ModelByName("agent" + std::to_string(i)))
+            common::Time::MSleep(100);
+
+        agent_model_array.push_back(this->world->ModelByName("agent" + std::to_string(i)));
     }
 
     void PedSimPlugin::OnUpdate(){
-        // Move all agents
-        // pedscene->moveAgents(0.3);
 
-        /*
-        for (int j = 0; j < this->agent_array.size(); j++) {
-            Ped::Tvector pos = agent_array[j]->getPosition();
-            ROS_INFO("Position of agent %d : (%f, %f, %f)", j, pos.x, pos.y, pos.z);
+        if (this->isPedSimInit) {
+            // Move all agents
+            pedscene->moveAgents(0.05);
+
+            for (int j = 0; j < this->agent_array.size(); j++) {
+                Ped::Tvector pos = agent_array[j]->getPosition();
+                agent_model_array[j]->SetWorldPose(ignition::math::Pose3d(pos.x, pos.y, pos.z, 0, 0, 0));
+            }
         }
-        */
+    }
+
+    // TODO: add reset service
+    // TODO: add params to init service to set number of agents, obstacles (?), waypoints, scene size and forces on agent
+
+    bool PedSimPlugin::PedSimInitService(
+        pedsim_ros_plugin::PedSimInit::Request &req, pedsim_ros_plugin::PedSimInit::Response &res)
+    {
+        ROS_INFO_STREAM("PedSimInitService called: " << req.data);
+        if (!this->isPedSimInit) {
+            this->initPedSim();
+            this->isPedSimInit = true;
+        }
+        return true;
     }
 
 } // namespace gazebo
